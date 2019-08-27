0ad03e0060bbd6df0c07b9b188ca254beb985936
Francisco Rojas <francisco.rojas.morales@gmail.com>
2019-08-26 22:15:13 -0400

    segundo intento

--- lwc-services.config.js
+++ lwc-services.config.js
@@ -1,7 +1,11 @@
 // Find the full example of all available configuration options at
 // https://github.com/muenzpraeger/lwc-create-app/blob/master/packages/lwc-services/example/lwc-services.config.js
 module.exports = {
-    resources: [{ from: 'src/client/resources', to: 'dist/resources' }],
+    resources: [{ from: 'src/client/resources', to: 'dist/resources' },
+    {
+        from: 'node_modules/@salesforce-ux/design-system/assets',
+        to: 'dist/resources/slds'
+    }],
     sourceDir: './src/client',
     moduleDir: './src/client/modules',
     server: {
